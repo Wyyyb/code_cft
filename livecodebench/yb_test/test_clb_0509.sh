fb1f46a54044d4f943fd8820b895288fb28dbbdf
y726wang <ubo.wang.sunny@gmail.com>
2025-05-11 19:43:01 -0400

    add

--- livecodebench/yb_test/test_clb_0509.sh
+++ livecodebench/yb_test/test_clb_0509.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-cd /data/yubo/cft/
+cd /data/yubo/code_cft/livecodebench
 
 python -m lcb_runner.runner.main \
     --model Qwen/Qwen3-4B \
@@ -10,7 +10,10 @@ python -m lcb_runner.runner.main \
     --local_model_path /data/yubo/models/Qwen3-4B \
     --scenario codegeneration \
     --evaluate \
-    --release_version release_v5
+    --release_version release_v5 \
+    --n 1 \
+    ----codegen_n 1 \
+    --output_dir ./output_result/Qwen3-4B_0511/
 
 
 
